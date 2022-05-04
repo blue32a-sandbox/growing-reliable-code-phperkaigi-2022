c9e5425cde6f89d44d6f99d0d70013c08b4b1c4e
blue32a <blue32a.jp@gmail.com>
2022-05-04 04:37:59 +0000

    誤った防御的プログラミング

--- src/BugRepository.php
+++ src/BugRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PhperKaig;
 
+use InvalidArgumentException;
 use PDO;
 
 class BugRepository
@@ -17,6 +18,33 @@ class BugRepository
 
     public function findAll($params)
     {
+        if (is_null($params)) {
+            throw new InvalidArgumentException('params should not be null');
+        }
+        if (!is_array($params)) {
+            throw new InvalidArgumentException('params should be an array');
+        }
+        if (count($params) !== 3) {
+            throw new InvalidArgumentException('params should have exact three items');
+        }
+        if (!array_key_exists('startAt', $params) ||
+            !array_key_exists('endAt', $params) ||
+            !array_key_exists('status', $params)) {
+            throw new InvalidArgumentException('params should have keys "startAt", "endAt" and "status"');
+        }
+        if (!is_string($params['startAt'])) {
+            throw new InvalidArgumentException('params["startAt"] should be a string');
+        }
+        if (!is_string($params['endAt'])) {
+            throw new InvalidArgumentException('params["endAt"] should be a string');
+        }
+        if (!is_string($params['status'])) {
+            throw new InvalidArgumentException('params["status"] should be a string');
+        }
+        if (!in_array($params['status'], ['OPEN', 'NEW', 'FIXED'], true)) {
+            throw new InvalidArgumentException('params["status"] should be in "OPEN","NEW","FIXED"');
+        }
+
         $sql = 'SELECT bug_id, summary, reported_at FROM Bugs
                 WHERE reported_at >= :startAt AND reported_at < :endAt
                 AND status = :status';
