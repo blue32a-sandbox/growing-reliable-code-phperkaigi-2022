e11f29487e13470e7a37aaec8d8d810e7506658a
blue32a <blue32a.jp@gmail.com>
2022-05-04 14:45:16 +0000

    日時期間の整合性を担保するため不変条件を追加する

--- src/DateTimeRange.php
+++ src/DateTimeRange.php
@@ -4,10 +4,23 @@ declare(strict_types=1);
 
 namespace PhperKaigi;
 
+use InvalidArgumentException;
+
 final class DateTimeRange
 {
     public function __construct(
         public readonly DateTimeEndpoint $startAt,
         public readonly DateTimeEndpoint $endAt,
-    ) {}
+    ) {
+        $startAtValue = $startAt->value();
+        $endAtValue = $endAt->value();
+        if ($startAtValue > $endAtValue) {
+            throw new InvalidArgumentException('startAt > endAt');
+        }
+        if ($startAtValue == $endAtValue) {
+            if (!$startAt->inclusive || $endAt->inclusive) {
+                throw new InvalidArgumentException('Both endpoints should be inclusive if startAt == endAt');
+            }
+        }
+    }
 }
---
+++ tests/DateTimeRangeTest.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests;
+
+use InvalidArgumentException;
+use PhperKaigi\DateTimeEndpoint;
+use PhperKaigi\DateTimeRange;
+use PHPUnit\Framework\TestCase;
+
+class DateTimeRangeTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function 終了日時が開始日時より前の場合は例外が発生する(): void
+    {
+        $startAt = DateTimeEndpoint::icluding('2021-12-25');
+        $endAt = DateTimeEndpoint::excluding('2021-12-24');
+
+        $this->expectException(InvalidArgumentException::class);
+        new DateTimeRange($startAt, $endAt);
+    }
+
+    /**
+     * @test
+     */
+    public function 上端点と下端点が同じ場合は例外が発生する(): void
+    {
+        $startAt = DateTimeEndpoint::icluding('2021-12-25');
+        $endAt = DateTimeEndpoint::icluding('2021-12-25');
+
+        $this->expectException(InvalidArgumentException::class);
+        new DateTimeRange($startAt, $endAt);
+    }
+}
